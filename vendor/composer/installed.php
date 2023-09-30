176acf7c192ada74eadc84e72eff18ed2c0f8616
fuadadhim24 <fuadadhim24@gmail.com>
2023-09-30 15:25:40 +0700

    add bootstrap and fixed ignored file vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'fuadadhim24/sfs-pertanian',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '9937bc604cb26f8bfa2d2538fb3008058ea8a18a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'fuadadhim24/sfs-pertanian' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '9937bc604cb26f8bfa2d2538fb3008058ea8a18a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
