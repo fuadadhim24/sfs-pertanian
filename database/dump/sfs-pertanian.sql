ee1e10653f75a898772d5ae80b6bec2a179fabdf
fuadadhim24 <fuadadhim24@gmail.com>
2023-09-30 15:54:37 +0700

    Update sfs-pertanian.sql

--- database/dump/sfs-pertanian.sql
+++ database/dump/sfs-pertanian.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 15 Sep 2023 pada 12.34
+-- Waktu pembuatan: 30 Sep 2023 pada 10.54
 -- Versi server: 10.4.28-MariaDB
 -- Versi PHP: 8.2.4
 
@@ -24,58 +24,83 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `activity`
+-- Struktur dari tabel `hasil_uji_tanah`
 --
 
-CREATE TABLE `activity` (
-  `id_activity` int(11) NOT NULL,
+CREATE TABLE `hasil_uji_tanah` (
+  `id_uji_tanah` int(11) NOT NULL,
+  `hasil_uji` text NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `informasi_cuaca`
+--
+
+CREATE TABLE `informasi_cuaca` (
+  `id_cuaca` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `cuaca_saat_ini` varchar(50) NOT NULL,
+  `prakiraan_cuaca` varchar(100) NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jadwal_pertanian`
+--
+
+CREATE TABLE `jadwal_pertanian` (
+  `id_jadwal` int(11) NOT NULL,
+  `tanggal_tanam` date NOT NULL,
+  `jenis_tanaman` varchar(50) NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `peralatan_pertanian`
+--
+
+CREATE TABLE `peralatan_pertanian` (
+  `id_produk` int(11) NOT NULL,
+  `nama_produk` varchar(50) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `harga` decimal(10,2) NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pesanan_konsumen`
+--
+
+CREATE TABLE `pesanan_konsumen` (
+  `id_pesanan` int(11) NOT NULL,
+  `tanggal_penesanan` date NOT NULL,
   `id_user` int(11) NOT NULL,
-  `date` date NOT NULL,
-  `deskripsi` varchar(255) NOT NULL
+  `id_produk` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `total_harga` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `commodity`
+-- Struktur dari tabel `ulasan_produk`
 --
 
-CREATE TABLE `commodity` (
-  `id_commodity` int(11) NOT NULL,
-  `nama_komoditas` varchar(100) NOT NULL,
-  `lokasi_semai` varchar(50) NOT NULL,
-  `tanggal_semai` date NOT NULL,
-  `jenis_pupuk` varchar(50) NOT NULL,
-  `jumlah_pupuk` int(11) NOT NULL,
-  `jenis_penanggulangan` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `quality_control`
---
-
-CREATE TABLE `quality_control` (
-  `id_qc` int(11) NOT NULL,
-  `id_commodity` int(11) NOT NULL,
-  `tanggal_qc` date NOT NULL,
-  `status` varchar(20) NOT NULL,
-  `hasil_pemeriksaan` varchar(255) NOT NULL,
-  `catatan` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `report`
---
-
-CREATE TABLE `report` (
-  `id_report` int(11) NOT NULL,
+CREATE TABLE `ulasan_produk` (
+  `id_ulasan` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
-  `tanggal_laporan` date NOT NULL,
-  `deskripsi` text NOT NULL
+  `id_produk` int(11) NOT NULL,
+  `isi_ulasan` text NOT NULL,
+  `peringkat` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -87,41 +112,72 @@ CREATE TABLE `report` (
 CREATE TABLE `users` (
   `id_user` int(11) NOT NULL,
   `nama` varchar(50) NOT NULL,
-  `hak_akses` varchar(20) NOT NULL,
-  `username` varchar(20) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `alamat` text NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `hak_akses` enum('Petani','Konsumen','Admin','') NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `tanggal_daftar` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id_user`, `nama`, `alamat`, `email`, `hak_akses`, `username`, `password`, `tanggal_daftar`) VALUES
+(3, '', '', 'fuadadhim24@gmail.com', 'Admin', 'fuadadhim24', '$2y$10$ySZ1sbJmG.BsfWaZ5Eii5eOF5KCjxbiTVxQwpq7b7iu/gYOri4DnK', 0),
+(4, '', '', 'caesar@gmail.com', 'Petani', 'caesar', '$2y$10$ijWJTjoRO.5H6sy3rrTKoeXeC6.P7NfQcx6/Kscw41aTeIuHCWV0.', 0),
+(6, '', '', 'cek@gmail.com', 'Petani', 'cek', '$2y$10$q6Sb5EvTHxCSt4yPtLkzj.xjPNNKtNw3YXrsdMIhOFdnJRHqz/are', 0),
+(7, '', '', 'boboy@gmail.com', 'Petani', 'boboy', '$2y$10$NgKifv4sCEibx1k1XC2ASu3z1nGdRJ0szDDq2WVkMCAPgUtT3PCEy', 0),
+(8, '', '', 'guntur@gmail.com', 'Petani', 'guntur', '$2y$10$85uML4kE64nBFSNFlt8/Pet.wHBQRnVyBsgkiYFCkZR4RdT1HIBdq', 0),
+(9, '', '', 'rizky@gmail.com', 'Petani', 'rizky', '$2y$10$fpNXDvUoqJY3l92Y8Zf9.OKQore5J6iD47Ck5aY0BVKghT3nFtmWi', 0);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `activity`
+-- Indeks untuk tabel `hasil_uji_tanah`
 --
-ALTER TABLE `activity`
-  ADD PRIMARY KEY (`id_activity`),
+ALTER TABLE `hasil_uji_tanah`
   ADD KEY `id_user` (`id_user`);
 
 --
--- Indeks untuk tabel `commodity`
+-- Indeks untuk tabel `informasi_cuaca`
 --
-ALTER TABLE `commodity`
-  ADD PRIMARY KEY (`id_commodity`);
-
---
--- Indeks untuk tabel `quality_control`
---
-ALTER TABLE `quality_control`
-  ADD PRIMARY KEY (`id_qc`),
-  ADD KEY `id_commodity` (`id_commodity`);
-
---
--- Indeks untuk tabel `report`
---
-ALTER TABLE `report`
-  ADD PRIMARY KEY (`id_report`),
+ALTER TABLE `informasi_cuaca`
+  ADD PRIMARY KEY (`id_cuaca`),
   ADD KEY `id_user` (`id_user`);
+
+--
+-- Indeks untuk tabel `jadwal_pertanian`
+--
+ALTER TABLE `jadwal_pertanian`
+  ADD PRIMARY KEY (`id_jadwal`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indeks untuk tabel `peralatan_pertanian`
+--
+ALTER TABLE `peralatan_pertanian`
+  ADD PRIMARY KEY (`id_produk`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indeks untuk tabel `pesanan_konsumen`
+--
+ALTER TABLE `pesanan_konsumen`
+  ADD PRIMARY KEY (`id_pesanan`),
+  ADD KEY `id_user` (`id_user`,`id_produk`),
+  ADD KEY `id_produk` (`id_produk`);
+
+--
+-- Indeks untuk tabel `ulasan_produk`
+--
+ALTER TABLE `ulasan_produk`
+  ADD PRIMARY KEY (`id_ulasan`),
+  ADD KEY `id_user` (`id_user`,`id_produk`),
+  ADD KEY `id_produk` (`id_produk`);
 
 --
 -- Indeks untuk tabel `users`
@@ -134,56 +190,82 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT untuk tabel `activity`
+-- AUTO_INCREMENT untuk tabel `informasi_cuaca`
 --
-ALTER TABLE `activity`
-  MODIFY `id_activity` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `informasi_cuaca`
+  MODIFY `id_cuaca` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `commodity`
+-- AUTO_INCREMENT untuk tabel `jadwal_pertanian`
 --
-ALTER TABLE `commodity`
-  MODIFY `id_commodity` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `jadwal_pertanian`
+  MODIFY `id_jadwal` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `quality_control`
+-- AUTO_INCREMENT untuk tabel `peralatan_pertanian`
 --
-ALTER TABLE `quality_control`
-  MODIFY `id_qc` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `peralatan_pertanian`
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `report`
+-- AUTO_INCREMENT untuk tabel `pesanan_konsumen`
 --
-ALTER TABLE `report`
-  MODIFY `id_report` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `pesanan_konsumen`
+  MODIFY `id_pesanan` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `ulasan_produk`
+--
+ALTER TABLE `ulasan_produk`
+  MODIFY `id_ulasan` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Ketidakleluasaan untuk tabel `activity`
+-- Ketidakleluasaan untuk tabel `hasil_uji_tanah`
 --
-ALTER TABLE `activity`
-  ADD CONSTRAINT `activity_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
+ALTER TABLE `hasil_uji_tanah`
+  ADD CONSTRAINT `hasil_uji_tanah_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
 
 --
--- Ketidakleluasaan untuk tabel `quality_control`
+-- Ketidakleluasaan untuk tabel `informasi_cuaca`
 --
-ALTER TABLE `quality_control`
-  ADD CONSTRAINT `quality_control_ibfk_1` FOREIGN KEY (`id_commodity`) REFERENCES `commodity` (`id_commodity`);
+ALTER TABLE `informasi_cuaca`
+  ADD CONSTRAINT `informasi_cuaca_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
 
 --
--- Ketidakleluasaan untuk tabel `report`
+-- Ketidakleluasaan untuk tabel `jadwal_pertanian`
 --
-ALTER TABLE `report`
-  ADD CONSTRAINT `report_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
+ALTER TABLE `jadwal_pertanian`
+  ADD CONSTRAINT `jadwal_pertanian_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
+
+--
+-- Ketidakleluasaan untuk tabel `peralatan_pertanian`
+--
+ALTER TABLE `peralatan_pertanian`
+  ADD CONSTRAINT `peralatan_pertanian_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
+
+--
+-- Ketidakleluasaan untuk tabel `pesanan_konsumen`
+--
+ALTER TABLE `pesanan_konsumen`
+  ADD CONSTRAINT `pesanan_konsumen_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`),
+  ADD CONSTRAINT `pesanan_konsumen_ibfk_2` FOREIGN KEY (`id_produk`) REFERENCES `peralatan_pertanian` (`id_produk`);
+
+--
+-- Ketidakleluasaan untuk tabel `ulasan_produk`
+--
+ALTER TABLE `ulasan_produk`
+  ADD CONSTRAINT `ulasan_produk_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`),
+  ADD CONSTRAINT `ulasan_produk_ibfk_2` FOREIGN KEY (`id_produk`) REFERENCES `peralatan_pertanian` (`id_produk`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
